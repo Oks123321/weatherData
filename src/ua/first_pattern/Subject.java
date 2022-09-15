e4ef85bf45ee482aab1f2dda35491f6c6e99ac25
oks123321 <golubenkoo@gmail.com>
2022-09-15 15:51:02 +0300

    changed jdk to 1.8

--- src/ua/first_pattern/Subject.java
+++ src/ua/first_pattern/Subject.java
@@ -1,7 +1,6 @@
 package ua.first_pattern;
 
-import java.io.ObjectStreamException;
-import java.util.Observer;
+
 
 public interface Subject {
     public void registerObserver(Observer o);
--- src/ua/first_pattern/WeatherData.java
+++ src/ua/first_pattern/WeatherData.java
@@ -1,7 +1,7 @@
 package ua.first_pattern;
 
+
 import java.util.ArrayList;
-import java.util.Observer;
 
 public class WeatherData implements Subject{
    private ArrayList observers;
@@ -13,13 +13,13 @@ public class WeatherData implements Subject{
         observers = new ArrayList();
     }
 
-    @Override
+
     public void registerObserver(Observer o) {
         observers.add(o);
 
     }
 
-    @Override
+
     public void removeObserver(Observer o) {
 int i = observers.indexOf(o);
 if (i>=0){
@@ -29,6 +29,22 @@ if (i>=0){
 
     @Override
     public void notifyObservers() {
+        for (int i = 0; i<observers.size();i++){
+            Observer observer = (Observer) observers.get(i);
+            observer.update(temperature, humidity, pressure);
+        }
 
     }
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
 }
+
